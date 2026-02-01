bfc057cb8e39001cba2271226a16314964723eb1
derrick <dcravenus@gmail.com>
2026-02-01 14:56:58 -0700

    Add more functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,68 @@ import { DateTime } from "luxon";
 
 import TimeCard from "./TimeCard";
 
+import "./App.css";
+
 function App() {
-  const [dateTimes, setDateTimes] = useState([""]);
+  const [dateTimes, setDateTimes] = useState<Set<string>>(new Set([""]));
   const [inputValue, setInputValue] = useState("");
+  const [invalidInput, setInvalidInput] = useState(false);
+
+  const handleClose = (dateTime: string) => {
+    setDateTimes((prev) => {
+      const next = new Set(prev);
+      next.delete(dateTime);
+      return next;
+    });
+  };
 
   return (
-    <>
+    <div className="app-container">
       <h1>Timestamper</h1>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => {
-          setInputValue(e.target.value);
-        }}
-      ></input>
-      <button
-        onClick={() => {
-          if (inputValue && !DateTime.fromISO(inputValue).isValid) {
-            // TODO: Notify user date is invalid
-            return;
-          }
-          setDateTimes((prev) => {
-            return [...prev, inputValue];
-          });
-          setInputValue("");
-        }}
-      >
-        Add
-      </button>
-      {dateTimes.map((dateTime) => (
-        <TimeCard dateTime={dateTime}></TimeCard>
-      ))}
-    </>
+      <div className="input-container">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+          onInput={() => {
+            setInvalidInput(false);
+          }}
+          className={invalidInput ? "invalid" : ""}
+        ></input>
+        <button
+          onClick={() => {
+            if (inputValue && !DateTime.fromISO(inputValue).isValid) {
+              setInvalidInput(true);
+              return;
+            }
+            setDateTimes((prev) => {
+              const next = new Set(prev);
+              next.add(inputValue);
+              return next;
+            });
+            setInputValue("");
+          }}
+        >
+          Add
+        </button>
+      </div>
+
+      <div className="time-card-container">
+        {Array.from(dateTimes)
+          .map((dateTime) => {
+            return (
+              <TimeCard
+                dateTime={dateTime}
+                key={dateTime}
+                handleClose={handleClose}
+              ></TimeCard>
+            );
+          })
+          .reverse()}
+      </div>
+    </div>
   );
 }
 
--- src/TimeCard.tsx
+++ src/TimeCard.tsx
@@ -1,13 +1,30 @@
 import { getStringFromDateTime } from "./helpers";
 import { DateTime } from "luxon";
+import "./TimeCard.css";
 
-const TimeCard = ({ dateTime }: { dateTime?: DateTime | string }) => {
+const TimeCard = ({
+  dateTime,
+  handleClose,
+}: {
+  dateTime?: DateTime | string;
+  handleClose: any;
+}) => {
   return (
-    <div>
-      <p>Local Time ({getStringFromDateTime({ dateTime, timezone: true })})</p>
+    <div className="container">
+      <strong>
+        Local Time ({getStringFromDateTime({ dateTime, timezone: true })})
+      </strong>{" "}
+      <button
+        onClick={() => {
+          handleClose(dateTime);
+        }}
+      >
+        X
+      </button>
       <p>{getStringFromDateTime({ dateTime })}</p>
       <p>{getStringFromDateTime({ dateTime, iso: true })}</p>
-      <p>UTC Time</p>
+      <p></p>
+      <strong>UTC Time</strong>
       <p>{getStringFromDateTime({ dateTime, utc: true })}</p>
       <p>{getStringFromDateTime({ dateTime, utc: true, iso: true })}</p>
     </div>
